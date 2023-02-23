067cce4737e0c234ee756438fcc10b92e094cc36
Rahul Tasker <rahul.tasker@oracle.com>
2023-02-23 12:53:00 -0600

    update edge table names so users do not have to flip edges in graph studio

--- terraform/scripts/setconstraints.sql
+++ terraform/scripts/setconstraints.sql
@@ -5,5 +5,5 @@ update bank_txns set txn_id = rownum;
 commit;
 
 alter table bank_txns add primary key (txn_id);
-alter table bank_txns modify from_acct_id references bank_accounts (acct_id);
-alter table bank_txns modify to_acct_id references bank_accounts (acct_id);
+alter table bank_txns modify src_acct_id references bank_accounts (acct_id);
+alter table bank_txns modify dst_acct_id references bank_accounts (acct_id);
